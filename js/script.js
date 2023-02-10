715a8784348b9fb68b894c70ec13c2127ae3ef0d
Maxim Klochkov <maxim.klochkov@tmogroup.asia>
2023-02-10 14:08:44 +0300

    Added sticky behaviour for Header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+const header = document.getElementById('header');
+const sections = document.querySelectorAll('[data-section]')
+
+const onIntersect = (entries) => {
+    entries.forEach((entry) => {
+        // console.log('window.scrollY', window.scrollY)
+        window.scrollY > 0
+            ? header.classList.add('-sticky')
+            : header.classList.remove('-sticky')
+    })
+}
+
+const observer = new IntersectionObserver(onIntersect, {
+    threshold: 1
+})
+
+sections.forEach((section) => {
+    observer.observe(section)
+})
